7bc2804dd7dbb79097f1f3847e805b9c66d6ab7e
hli37 <72523884+hli37@users.noreply.github.com>
2021-09-28 22:34:49 -0700

    Add files via upload

---
+++ src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,104 @@
+package com.codepath.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.flixster.DetailActivity;
+import com.codepath.flixster.R;
+import com.codepath.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View moiveView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(moiveView);
+    }
+
+    // Involves populating data into the item through the holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHilder" + position);
+        // Get the movie at the passed in position
+        Movie movie = movies.get(position);
+        // Bind the movie data into the ViewHolder
+        holder.bind(movie);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            String imageUrl;
+            //if phone is in landscape
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            } else{
+                //else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_launcher_background).into(ivPoster);
+            // 1. Register click listener on the whole row
+            // 2. Navigate to a new activity on tap
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+}
